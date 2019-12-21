d2f23c6e5769a5bdc38dbd0350a874d13ed54081
SergeiLomako <flameseeker23@gmail.com>
2019-12-21 11:12:11 +0200

    feat(): added http interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor, ErrorInterceptor } from './interceptors';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
@@ -16,7 +16,18 @@ import { HeaderComponent } from './components/layout/header/header.component';
         AppRoutingModule,
         HttpClientModule,
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: JwtInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorInterceptor,
+            multi: true
+        },
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Joke, Comment, User } from '../models';
-import { AuthService } from './auth.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,28 +9,15 @@ import { AuthService } from './auth.service';
 
 export class ApiService {
     apiUrl: string = environment.apiUrl;
-    httpOptions;
 
-    constructor(
-        private httpClient: HttpClient,
-        private authService: AuthService
-    ) {
-        const authUser = authService.getAuthUser();
-        this.httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type':  'application/json',
-                Accept: 'application/json',
-                Authorization: `Bearer ${authUser.hasOwnProperty('access_token') && authUser.access_token}`
-            })
-        };
-    }
+    constructor(private httpClient: HttpClient) {}
 
     register(user: User) {
-        return this.httpClient.post(`${this.apiUrl}/register`, user, this.httpOptions);
+        return this.httpClient.post(`${this.apiUrl}/register`, user);
     }
 
     login(email: string, password: string) {
-        return this.httpClient.post(`${this.apiUrl}/login`, { email, password }, this.httpOptions)
+        return this.httpClient.post(`${this.apiUrl}/login`, { email, password });
     }
 
     getJokes({
@@ -39,30 +25,30 @@ export class ApiService {
         limit = environment.limit,
         search = ''
     }) {
-        return this.httpClient.get(`${this.apiUrl}/jokes?page=${page}&limit=${limit}&search=${search}`, this.httpOptions);
+        return this.httpClient.get(`${this.apiUrl}/jokes?page=${page}&limit=${limit}&search=${search}`);
     }
 
     getJokeById(id: string) {
-        return this.httpClient.get(`${this.apiUrl}/jokes/${id}`, this.httpOptions);
+        return this.httpClient.get(`${this.apiUrl}/jokes/${id}`);
     }
 
     createJoke(joke: Joke) {
-        return this.httpClient.post(`${this.apiUrl}/jokes`, joke, this.httpOptions);
+        return this.httpClient.post(`${this.apiUrl}/jokes`, joke);
     }
 
     updateJoke(joke: Joke) {
-        return this.httpClient.put(`${this.apiUrl}/jokes/${joke._id}`, joke, this.httpOptions);
+        return this.httpClient.put(`${this.apiUrl}/jokes/${joke._id}`, joke);
     }
 
     deleteJoke(id: string) {
-        return this.httpClient.delete(`${this.apiUrl}/jokes/${id}`, this.httpOptions);
+        return this.httpClient.delete(`${this.apiUrl}/jokes/${id}`);
     }
 
     createComment(comment: Comment) {
-        return this.httpClient.post(`${this.apiUrl}/comments`, comment, this.httpOptions);
+        return this.httpClient.post(`${this.apiUrl}/comments`, comment);
     }
 
     updateComment(comment: Comment) {
-        return this.httpClient.put(`${this.apiUrl}/comments/${comment._id}`, comment, this.httpOptions);
+        return this.httpClient.put(`${this.apiUrl}/comments/${comment._id}`, comment);
     }
 }
